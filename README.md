1eba7bf65a3fa140a7ca0b1beb28840a4d7db9a4
Gilles CRUCHON <gilles.cruchon@gmail.com>
2023-01-29 22:01:49 +0100

    feat: add logo overlay

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# qr-code-generator
+# QR Code Generator
 
-QR Code Generator
+A short utility to build custom QR Codes
 
 ## Install
 
@@ -13,3 +13,20 @@ pipenv install
 ```zsh
 pipenv run python qr.py -t https://www.url-to-encode.com
 ```
+
+Optionally, you can specify a name for your QR code:
+
+```zsh
+pipenv run python qr.py -t https://www.url-to-encode.com -o my_qr_code.png
+```
+
+Optionally, you can add a logo on top of QR code:
+
+```zsh
+pipenv run python qr.py -t https://www.url-to-encode.com -l logo.png
+```
+
+Please note that:
+
+- QR codes are generated in the `output` folder.
+- logo file should be in the `logos` folder.
--- qr.py
+++ qr.py
@@ -1,36 +1,70 @@
 import qrcode
 from qrcode.image.styledpil import StyledPilImage
 from qrcode.image.styles.moduledrawers import RoundedModuleDrawer
-from qrcode.image.styles.colormasks import RadialGradiantColorMask
+from qrcode.image.styles.colormasks import SquareGradiantColorMask
 import sys, getopt
 from os import path
+from PIL import Image, ImageDraw
+from math import sqrt
+
+BASE_WIDTH = 100
+HIGH_CORRECTION = qrcode.constants.ERROR_CORRECT_H
 
 
-def get_qr_img(text):
-    qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_L)
+def add_resized_logo(qr_img, logo_path):
+    print("- Adding logo:", logo_path)
+    try:
+        logo_img = Image.open(logo_path)
+        wpercent = BASE_WIDTH / float(logo_img.size[0])
+        hsize = int((float(logo_img.size[1]) * float(wpercent)))
+        logo_img = logo_img.resize((BASE_WIDTH, hsize), Image.Resampling.LANCZOS)
+
+        top_left_logo_position = (
+            (qr_img.size[0] - logo_img.size[0]) // 2,
+            (qr_img.size[1] - logo_img.size[1]) // 2,
+        )
+
+        qr_img.paste(logo_img, top_left_logo_position, logo_img)
+    except FileNotFoundError:
+        print("- Logo not found, step ignored")
+
+    return qr_img
+
+
+def get_qr_img(text, logo_file):
+    qr = qrcode.QRCode(error_correction=HIGH_CORRECTION)
     qr.add_data(text)
 
     blue = (0, 0, 255)
     red = (255, 0, 0)
 
-    return qr.make_image(
+    qr_img = qr.make_image(
         image_factory=StyledPilImage,
-        color_mask=RadialGradiantColorMask(center_color=blue, edge_color=red),
+        eye_drawer=RoundedModuleDrawer(radius_ratio=1.5),
+        module_drawer=RoundedModuleDrawer(),
+        color_mask=SquareGradiantColorMask(center_color=blue, edge_color=red),
     )
+
+    if logo_file != None:
+        qr_img = add_resized_logo(qr_img, path.join("logos", logo_file))
+
+    return qr_img
 
 
 def is_allowed(char):
-    return char.isalnum() or char in ("-", "_")
+    return char.isalnum() or char in ("-", "_", ".")
 
 
 def clean_filename(file_name):
-    return "".join(c for c in file_name if is_allowed(c))
+    file_name_allowed_chars = "".join(c for c in file_name if is_allowed(c))
+    return file_name_allowed_chars.replace("..", "")
 
 
 def main(argv):
     input_text = None
     output_file = "qr"
-    opts, args = getopt.getopt(argv, "ht:o:", ["text=", "output="])
+    logo_file = None
+    opts, args = getopt.getopt(argv, "ht:o:l:", ["text=", "output=", "logo="])
     for opt, arg in opts:
         if opt == "-h":
             print("qr.py -t <input_text> -o <output_file>")
@@ -39,14 +73,19 @@ def main(argv):
             input_text = arg.strip()
         elif opt in ("-o", "--output"):
             output_file = clean_filename(arg.strip())
+        elif opt in ("-l", "--logo"):
+            logo_file = clean_filename(arg.strip())
 
     if len(output_file) == 0:
         print("Filename cannot be empty")
     elif len(input_text) > 0:
         print("Generating QR Code for:", input_text)
-        img = get_qr_img(input_text)
-        print("Saving it at: {}.png".format(output_file))
-        img.save(path.join(".", "output", "{}.png".format(output_file)))
+        img = get_qr_img(input_text, logo_file)
+
+        print("- Saving QR Code at: output/{}".format(output_file))
+        img.save(path.join(".", "output", "{}".format(output_file)))
+
+        print("DONE")
     else:
         print("You need to define an input text")
 
